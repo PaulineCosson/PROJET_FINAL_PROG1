c98dd9c370c340d7af29a805a0b1a3d47ce1f44d
PaulineCosson <cosson.pa@gmail.com>
2024-12-10 11:29:55 +0100

    derniers changement 03/12

---
+++ src/choix_case.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <iostream>
+
+int choix_case (std::array <char, 9> plateau);
--- src/main_projet_final.cpp
+++ src/main_projet_final.cpp
@@ -2,6 +2,8 @@
 #include "player.hpp"
 #include "game_board.hpp"
 #include "menu.hpp"
+#include "choix_case.hpp"
+
 
 int main (){
     //initialise un plateau vide
@@ -33,33 +35,7 @@ int main (){
                 symbol = player2.symbol;
             }
 
-            unsigned int numeroCase {};
-            while (true)
-            {
-                std::cout << "Choix de la case : " ;
-                std::cin >> numeroCase;
-
-                if (std::cin.fail())
-                {
-                    std::cin.clear(); // On remet std::cin dans un état fonctionnel.
-                    std::cin.ignore(255, '\n'); // On vide les caractères mémorisés.
-                    std::cout << "Erreur valeur non acceptee " << std::endl;
-                }    
-                else if (numeroCase == 0 || numeroCase>9)
-                {
-                    std::cin.ignore(255, '\n'); // On vide les caractères mémorisés.
-                    std::cout << "Erreur valeur non acceptee " << std::endl;
-                }
-                else if(plateau[numeroCase-1] != '.'){
-                    std::cin.ignore(255, '\n'); // On vide les caractères mémorisés.
-                    std::cout << "Case déjà prise " << std::endl;
-                }
-                else
-                {
-                    break;
-                }
-                
-            }
+            numeroCase = choixCase(plateau);
             plateau[numeroCase-1] = symbol;
             tour++;
         }
